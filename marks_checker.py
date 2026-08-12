ff72a82ccf1ee099994e5b928290381f1059456c
palaknagele <palaknagele4@gmail.com>
2026-08-12 10:54:00 +0530

    Create marks_checker.py

---
+++ marks_checker.py
@@ -0,0 +1,10 @@
+marks = int(input("Enter your marks: "))
+
+if marks >= 80:
+    print("Excellent")
+elif marks >= 60:
+    print("Good")
+elif marks >= 40:
+    print("Pass")
+else:
+    print("Fail")
